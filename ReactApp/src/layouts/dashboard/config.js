3109e219f7b79471b1a16c996194472637b68bac
t-morabo <t-morabo@microsoft.com>
2024-05-23 12:18:14 +0300

    adding create question based on existing appendix

--- ReactApp/src/layouts/dashboard/config.js
+++ ReactApp/src/layouts/dashboard/config.js
@@ -1,11 +1,8 @@
 import ChartBarIcon from '@heroicons/react/24/solid/ChartBarIcon';
 import CogIcon from '@heroicons/react/24/solid/CogIcon';
-import LockClosedIcon from '@heroicons/react/24/solid/LockClosedIcon';
 import ShoppingBagIcon from '@heroicons/react/24/solid/ShoppingBagIcon';
 import UserIcon from '@heroicons/react/24/solid/UserIcon';
-import UserPlusIcon from '@heroicons/react/24/solid/UserPlusIcon';
 import UsersIcon from '@heroicons/react/24/solid/UsersIcon';
-import XCircleIcon from '@heroicons/react/24/solid/XCircleIcon';
 import { SvgIcon } from '@mui/material';
 
 export const items = [
@@ -32,11 +29,11 @@ export const items = [
       },
       {
         title: 'Appendix Meta-Question',
-        path: '/create/simple',
+        path: '/create/choose-appendix',
       },
       {
         title: 'Appendix plus Meta-Question',
-        path: '/create/appendix',
+        path: '/create/appendix-plus-question',
       }
     ]
   },
