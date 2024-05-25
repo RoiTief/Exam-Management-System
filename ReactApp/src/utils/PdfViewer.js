596e9c09d795e6c3ff019200a15fdb7cebc999e7
Roi Tiefenbrunn <roitief@post.bgu.ac.il>
2024-05-25 20:11:29 +0300

    Update to PdfViewer

--- ReactApp/src/utils/PdfViewer.js
+++ ReactApp/src/utils/PdfViewer.js
@@ -39,17 +39,20 @@ const PdfViewer = () => {
   };
 
   return (
-    <div style={{ height: '100vh' }}>
-      {loading ? (
+    <div>
+    { loading ? (
         <p>Loading PDF...</p>
       ) : (
-        <embed
-          src={pdfUrl}
-          type="application/pdf"
-          width="100%"
-          height="100%"
-        />
-      )}
+        <div style={{ height: '100vh' }}>
+          <embed
+            src={pdfUrl}
+            type="application/pdf"
+            width="100%"
+            height="100%"
+          />
+        </div>
+      )
+    }
     </div>
   );
 };
