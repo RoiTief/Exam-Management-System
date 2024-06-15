3f3684ab8c373ce232f24f8636260e4228cc18d7
RoiTief <80453712+RoiTief@users.noreply.github.com>
2024-06-15 20:24:50 +0300

    Add logic to edit users through UserController (#61)

--- src/main/ErrorMessages.js
+++ src/main/ErrorMessages.js
@@ -1,5 +1,6 @@
 // Error messages
 
+const {USER_TYPES} = require("./Enums");
 /**
  * Holds all error messages that might occur in user processes such as register/login
  */
@@ -15,6 +16,7 @@ const USER_PROCESS_ERROR_MSGS = {
     USER_DETAILS_MISSING_LNAME: 'Last name field is required',
     USER_DETAILS_MISSING_EMAIL: 'Email field is required',
     USER_DETAILS_MISSING_TYPE: 'UserType field is required',
+    INVALID_TYPE: type => `User type '${type}' is invalid, use one of '${Object.values(USER_TYPES).join(', ')}'`,
     INCORRECT_TYPE : (realType, expectedType) => `expected user type '${expectedType}', but got '${realType}'`,
 
     //Login
